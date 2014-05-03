c5ffc5d495cbb43c71d35204d9aa61ee4b22c4c6
Peter Butkovic <butkovic@gmail.com>
2014-05-03 20:11:51 +0200

    propagating system properties to in-container tests via properties file

--- src/test/java/com/p6spy/engine/spy/P6SpyTestBase.java
+++ src/test/java/com/p6spy/engine/spy/P6SpyTestBase.java
@@ -5,6 +5,7 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 import org.jboss.shrinkwrap.api.ShrinkWrap;
@@ -12,35 +13,78 @@ import org.jboss.shrinkwrap.api.asset.FileAsset;
 import org.jboss.shrinkwrap.api.gradle.archive.importer.embedded.EmbeddedGradleImporter;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.BeforeClass;
+import org.springframework.beans.factory.annotation.Value;
 
 public abstract class P6SpyTestBase {
 
-  public static final File SPY_LOG = new File(/*System.getProperty("logDir"), */"spy.it.log");
+	public static final File SPY_LOG = new File(/*
+												 * System.getProperty("buildDir")
+												 * ,
+												 */"spy.it.log");
 
-  @BeforeClass
-  public static void beforeClass() throws IOException {
-    SPY_LOG.delete();
-    assertThat(SPY_LOG.exists(), is(equalTo(false)));
-  }
+	@BeforeClass
+	public static void beforeClass() throws IOException {
+		SPY_LOG.delete();
+		assertThat(SPY_LOG.exists(), is(equalTo(false)));
+	}
 
-  public static WebArchive createDeployment() {
-    return ShrinkWrap.create(EmbeddedGradleImporter.class)
-        .forThisProjectDirectory()
-        // rather using env variable: export ORG_GRADLE_PROJECT_container=wildfly81x ; ./gradlew clean unitTest
-        //.forTasks("build")
-        //.withArguments("-x", "test", "-Pcontainer=jboss51x")
-        .importBuildOutput().as(WebArchive.class)
-        .addPackages(true, "com.p6spy.engine.spy")
-        .addAsWebInfResource(new FileAsset(new File("src/test/config/glassfish-web.Driver.xml")), "glassfish-web.xml")
-        ;
-  }
-  
-  protected static boolean isJBossOrWildFly() {
-    return System.getProperty("arquillian.launch").contains("jboss") || System.getProperty("arquillian.launch").contains("wildfly");
-  }
-  
-  protected static boolean isJBoss42x() {
-    return System.getProperty("arquillian.launch").equals("jboss42x");
-  }
+	public static WebArchive createDeployment() {
+		return ShrinkWrap
+				.create(EmbeddedGradleImporter.class)
+				.forThisProjectDirectory()
+				// rather using env variable: export
+				// ORG_GRADLE_PROJECT_container=wildfly81x ; ./gradlew clean
+				// unitTest
+				// .forTasks("build")
+				// .withArguments("-x", "test", "-Pcontainer=jboss51x")
+				.importBuildOutput()
+				.as(WebArchive.class)
+				.addPackages(true, "com.p6spy.engine.spy")
+				.addAsWebInfResource(
+						new FileAsset(new File(
+								"src/test/config/glassfish-web.Driver.xml")),
+						"glassfish-web.xml")
 
+				// propagate system properties to in-container junit tests
+				.addAsResource(getFileDumpedSystemProperties());
+	}
+
+	/**
+	 * @return (temporary) properties file holding dumped system properties.
+	 */
+	private static File getFileDumpedSystemProperties() {
+		final File file = new File("build/system.properties");
+		file.deleteOnExit();
+		FileOutputStream fos = null;
+		try {
+			fos = new FileOutputStream(file);
+			System.getProperties().store(fos, "");
+		} catch (IOException e) {
+			if (null != fos) {
+				try {
+					fos.close();
+				} catch (IOException e1) {
+				}
+			}
+			e.printStackTrace();
+		}
+		return file;
+	}
+
+	protected static boolean isJBossOrWildFly() {
+		return System.getProperty("arquillian.launch").contains("jboss")
+				|| System.getProperty("arquillian.launch").contains("wildfly");
+	}
+
+	protected static boolean isJBoss42x() {
+		return System.getProperty("arquillian.launch").equals("jboss42x");
+	}
+
+	// injected in the container
+	@Value("${buildDir}")
+    private String buildDir;
+	
+	protected String getBuildDir() {
+		return buildDir;
+	}
 }
